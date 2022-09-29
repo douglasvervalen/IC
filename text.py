ec6c8c71637b5a7824b1438c103cac1e130f31a8
douglasvervalen <69044467+douglasvervalen@users.noreply.github.com>
2022-09-29 02:59:41 -0300

    Update text.py

--- text.py
+++ text.py
@@ -2,6 +2,9 @@ n = '\n'
 
 def gen_membrane(L, W, path, j):
     text = (
+    f'package require pbctools {n}'
+    f'package require topotools {n}'
+    f'package require nanotube {n}'
     f'set fname {W}x{L[j]}.pdb {n}'
     f'set fdata {W}x{L[j]}.full {n}'
     f'set my_lx {W} {n}'
@@ -12,12 +15,12 @@ def gen_membrane(L, W, path, j):
     f'$sel move [transaxis z 90] {n}'
     f'set box {{{2 * L[j]} {2 * L[j]} {2 * L[j]} 90 90 90}} {n}'
     f'pbc set $box {n}'
-    f'source /Users/kyoun/IC/sculptor.tcl {n}'
+    f'source /home/woellner/IC/Douglas/estruturas/sculptor.tcl {n}'
     f'::Sculptor::sculpt \"{path}\" {n}'
-    f'set center [measure center $sel weight none] {n}'
+    f'set center [measure center $sel weight none] {n}' 
     f'$sel moveby [vecscale -1.0 $center] {n}'
-    f'topo writelammpsdata /Users/kyoun/IC/estruturas/data/$fdata full {n}'
-    f'$sel writepdb /Users/kyoun/IC/estruturas/pdb/$fname {n}'
+    f'topo writelammpsdata /home/woellner/IC/Douglas/estruturas/data/$fdata full {n}'
+    f'$sel writepdb/home/woellner/IC/Douglas/estruturas/pdb/$fname {n}'
     )
     return text
 
